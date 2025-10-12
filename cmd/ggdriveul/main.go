45abda2776ac59830f5feb98a539055c0f7f52fe
sunshine55 <tony.wu90@yahoo.com>
2025-10-11 23:24:44 -0500

    list personal drive files using service account

--- cmd/ggdriveul/main.go
+++ cmd/ggdriveul/main.go
@@ -1,1 +1,33 @@
-package ggdriveul
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"google.golang.org/api/drive/v3"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	ctx := context.Background()
+
+	srv, err := drive.NewService(ctx, option.WithCredentialsFile("credentials.json"))
+	if err != nil {
+		log.Fatalf("Unable to create Drive client: %v", err)
+	}
+
+	r, err := srv.Files.List().PageSize(10).
+		Fields("nextPageToken, files(id, name)").Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve files: %v", err)
+	}
+	fmt.Println("Files:")
+	if len(r.Files) == 0 {
+		fmt.Println("No files found.")
+	} else {
+		for _, i := range r.Files {
+			fmt.Printf("%s (%s)\n", i.Name, i.Id)
+		}
+	}
+}
